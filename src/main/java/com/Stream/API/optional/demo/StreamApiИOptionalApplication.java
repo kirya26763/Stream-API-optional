95d4a8a6ecee603c10d32548285ed69f0387c2a9
Arty <kirya26763@mail.ru>
2025-04-08 19:11:25 +0700

    init

---
+++ src/main/java/com/Stream/API/optional/demo/StreamApiИOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.Stream.API.optional.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiИOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiИOptionalApplication.class, args);
+	}
+
+}
